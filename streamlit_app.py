cada68219bad41d99540f20071cbcc34fe2e8e98
Lourence <rencekruger@gmail.com>
2023-10-11 22:40:23 +1300

    Update streamlit_app.py
    t13

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Kale🐍')
-streamlit.text('Eggs😋')
+streamlit.text('Kale  🐍')
+streamlit.text('Eggs  😋')
 streamlit.header('Build your own 😛')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
