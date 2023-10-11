447b44bee117a0fec5e165cfd4873b59b77b6c3f
Lourence <rencekruger@gmail.com>
2023-10-11 22:28:50 +1300

    Update streamlit_app.py
    t4

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,11 @@
 
-import streamlit,unicodedata
-from browser import document
-from browser import html
+import streamlit
+
 streamlit.title('My Parents New Healthy Diner')
+streamlit.text('Breakfast Menu')
+streamlit.text('Kale')
+streamlit.text('Eggs')
 
 
-# Navigation
-nav = html.DIV('Python 🐍', Class="nav")
-
-# Content
-cnt = html.DIV('You can do really awesome stuff using a nice slack!', Class="content")
-
-# Footer
-ftr = html.DIV('Made with luv by: Cabral', Class="footer")
-
-# Push 
-document <= [nav, cnt, ftr]
 
 
