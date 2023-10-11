348f504552149c7f5610354931c3e304b4775c58
Lourence <rencekruger@gmail.com>
2023-10-11 22:40:03 +1300

    Update streamlit_app.py
    t12

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-import pandas
+
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -7,6 +7,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Kale🐍')
 streamlit.text('Eggs😋')
 streamlit.header('Build your own 😛')
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
